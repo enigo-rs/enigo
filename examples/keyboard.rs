4378a77863cff041370ed8b472cb5c5d721d320c
Dustin Bensing <dustin.bensing@googlemail.com>
2017-06-04 15:28:36 +0200

    fixed example

--- examples/keyboard.rs
+++ examples/keyboard.rs
@@ -6,6 +6,5 @@ fn main() {
     let wait_time = time::Duration::from_millis(2000);
     let mut enigo = Enigo::new();
 
-    //enigo.key_sequence("Hello World!");
-    enigo.key_click(Key::LAYOUT("a".into()));
+    enigo.key_sequence("Hello World!");
 }
