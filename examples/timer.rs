141537296017ba6e01bc8027741d55327f701c5b
jD91mZM2 <me@krake.one>
2017-10-25 17:25:48 +0200

    Timer example

---
+++ examples/timer.rs
@@ -0,0 +1,23 @@
+extern crate enigo;
+use enigo::{Enigo, KeyboardControllable, Key};
+use std::thread;
+use std::time::Duration;
+use std::time::Instant;
+
+fn main() {
+    thread::sleep(Duration::from_secs(2));
+    let mut enigo = Enigo::new();
+
+    let now = Instant::now();
+
+    // write text
+    enigo.key_sequence("Hello World! ❤️");
+
+    let time = now.elapsed();
+    println!("{:?}", time);
+
+    // select all
+    enigo.key_down(Key::Control);
+    enigo.key_click(Key::Layout('a'));
+    enigo.key_up(Key::Control);
+}
