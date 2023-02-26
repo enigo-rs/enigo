3d79690d2001e99152425545ab69f55c1e6e8165
pentamassiv <pentamassiv@posteo.de>
2023-02-26 21:56:50 +0100

    Improve the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,26 @@
 //! Enigo lets you simulate mouse and keyboard input-events as if they were
-//! made by the actual hardware. The goal is to make it available on different
-//! operating systems like Linux, macOS and Windows – possibly many more but
-//! [Redox](https://redox-os.org/) and *BSD are planned. Please see the
-//! [Repo](https://github.com/enigo-rs/enigo) for the current status.
+//! made by the actual hardware. It is available on Linux (X11), macOS and
+//! Windows.
 //!
-//! I consider this library in an early alpha status, the API will change in
-//! in the future. The keyboard handling is far from being very usable. I plan
-//! to build a simple
-//! [DSL](https://en.wikipedia.org/wiki/Domain-specific_language)
-//! that will resemble something like:
+//! It can be used for testing user interfaces on different platforms, building
+//! remote control applications or just automating tasks for user interfaces
+//! unaccessible by a public API or scripting language.
 //!
-//! `"hello {+SHIFT}world{-SHIFT} and break line{ENTER}"`
+//! This library is in an early alpha status, the API will change in
+//! in the future.
 //!
-//! The current status is that you can just print
-//! [unicode](http://unicode.org/)
-//! characters like [emoji](http://getemoji.com/) without the `{+SHIFT}`
-//! [DSL](https://en.wikipedia.org/wiki/Domain-specific_language)
-//! or any other "special" key on the Linux, macOS and Windows operating system.
+//! In order to use the library, you only have to know about three
+//! things:
+//! - [KeyboardControllable] (trait): used to simulate a key click, enter text
+//!   or something similar
+//! - [MouseControllable] (trait): do something with the mouse or you find out
+//!   the display
+//! size
+//! - [Enigo] (struct): implements the two traits [KeyboardControllable] and
+//!   [MouseControllable]
 //!
-//! Possible use cases could be for testing user interfaces on different
-//! platforms,
-//! building remote control applications or just automating tasks for user
-//! interfaces unaccessible by a public API or scripting language.
-//!
-//! For the keyboard there are currently two modes you can use. The first mode
-//! is represented by the [`key_sequence`](KeyboardControllable::key_sequence)
-//! function its purpose is to simply write unicode characters. This is
-//! independent of the keyboard layout. Please note that
-//! you're not be able to use modifier keys like Control
-//! to influence the outcome. If you want to use modifier keys to e.g.
-//! copy/paste, use the Layout variant. Please note that this is indeed layout
-//! dependent.
+//! A simple [DSL](https://en.wikipedia.org/wiki/Domain-specific_language)
+//! is available. It is documented in the [`dsl`] module.
 
 //! # Examples
 //! ```no_run
@@ -51,6 +41,7 @@
 //! enigo.mouse_up(MouseButton::Left);
 //! enigo.key_sequence("hello world");
 //! ```
+
 #![deny(clippy::pedantic)]
 #![allow(clippy::cast_lossless)]
 #![allow(clippy::cast_possible_truncation)]
@@ -80,6 +71,9 @@ mod linux;
 pub use crate::linux::Enigo;
 
 /// DSL parser module
+///
+/// The current status is that you can just print [unicode](http://unicode.org/) characters like [emoji](http://getemoji.com/) without the `{+SHIFT}`
+/// [DSL](https://en.wikipedia.org/wiki/Domain-specific_language) or any other "special" key on the Linux, macOS and Windows operating system.
 pub mod dsl;
 
 #[cfg(feature = "with_serde")]
@@ -91,12 +85,8 @@ extern crate serde;
 
 #[cfg_attr(feature = "with_serde", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-/// [`MouseButton`] represents a mouse button,
-/// and is used in for example
+/// [`MouseButton`] represents a mouse button and is used in e.g
 /// [`MouseControllable::mouse_click`].
-/// WARNING: Types with the prefix Scroll
-/// IS NOT intended to be used, and may not work on
-/// all operating systems.
 pub enum MouseButton {
     /// Left mouse button
     Left,
@@ -105,20 +95,28 @@ pub enum MouseButton {
     /// Right mouse button
     Right,
 
-    /// Scroll up button
+    /// Scroll up button. It is better to use the
+    /// [MouseControllable::mouse_scroll_y] method to scroll.
     ScrollUp,
-    /// Left right button
+    /// Scroll down button. It is better to use the
+    /// [MouseControllable::mouse_scroll_y] method to scroll.
     ScrollDown,
-    /// Left right button
+    /// Scroll left button. It is better to use the
+    /// [MouseControllable::mouse_scroll_x] method to scroll.
     ScrollLeft,
-    /// Left right button
+    /// Scroll right button. It is better to use the
+    /// [MouseControllable::mouse_scroll_x] method to scroll.
     ScrollRight,
 }
 
-/// Representing an interface and a set of mouse functions every
-/// operating system implementation _should_ implement.
+/// Contains functions to control the mouse and to get the size of the display.
+/// Enigo uses a Cartesian coordinate system for specifying coordinates. The
+/// origin in this system is located in the top-left corner of the current
+/// screen, with positive values extending along the axes down and to the
+/// right of the origin point and it is measured in pixels. The same coordinate
+/// system is used on all operating systems.
 pub trait MouseControllable {
-    /// Lets the mouse cursor move to the specified x and y coordinates.
+    /// Move the mouse cursor to the specified x and y coordinates.
     ///
     /// The topleft corner of your monitor screen is x=0 y=0. Move
     /// the cursor down the screen by increasing the y and to the right
@@ -133,16 +131,11 @@ pub trait MouseControllable {
     /// ```
     fn mouse_move_to(&mut self, x: i32, y: i32);
 
-    /// Lets the mouse cursor move the specified amount in the x and y
-    /// direction.
-    ///
-    /// The amount specified in the x and y parameters are added to the
-    /// current location of the mouse cursor. A positive x values lets
-    /// the mouse cursor move an amount of `x` pixels to the right. A negative
-    /// value for `x` lets the mouse cursor go to the left. A positive value
-    /// of y
-    /// lets the mouse cursor go down, a negative one lets the mouse cursor go
-    /// up.
+    /// Move the mouse cursor the specified amount in the x and y
+    /// direction. A positive x value moves the mouse cursor `x` pixels to the
+    /// right. A negative value for `x` moves the mouse cursor to the left.
+    /// A positive value of y moves the mouse cursor down, a negative one
+    /// moves the mouse cursor up.
     ///
     /// # Example
     ///
@@ -153,12 +146,9 @@ pub trait MouseControllable {
     /// ```
     fn mouse_move_relative(&mut self, x: i32, y: i32);
 
-    /// Push down one of the mouse buttons
-    ///
-    /// Push down the mouse button specified by the parameter `button` of
-    /// type [`MouseButton`]
-    /// and holds it until it is released by
-    /// [`MouseControllable::mouse_up`](.
+    /// Push down the mouse button specified by the parameter
+    /// `button` of type [`MouseButton`] and hold it until it is released by
+    /// [`MouseControllable::mouse_up`].
     /// Calls to [`MouseControllable::mouse_move_to`] or
     /// [`MouseControllable::mouse_move_relative`] will
     /// work like expected and will e.g. drag widgets or highlight text.
@@ -172,7 +162,7 @@ pub trait MouseControllable {
     /// ```
     fn mouse_down(&mut self, button: MouseButton);
 
-    /// Lift up a pushed down mouse button
+    /// Release a pushed down mouse button
     ///
     /// Lift up a previously pushed down button (by invoking
     /// [`MouseControllable::mouse_down`]).
@@ -186,13 +176,14 @@ pub trait MouseControllable {
     /// ```no_run
     /// use enigo::*;
     /// let mut enigo = Enigo::new();
+    /// enigo.mouse_down(MouseButton::Right);
     /// enigo.mouse_up(MouseButton::Right);
     /// ```
     fn mouse_up(&mut self, button: MouseButton);
 
     /// Click a mouse button
     ///
-    /// it's essentially just a consecutive invocation of
+    /// It is essentially just a consecutive invocation of
     /// [`MouseControllable::mouse_down`]
     /// followed by a [`MouseControllable::mouse_up`]. Just for
     /// convenience.
@@ -208,11 +199,11 @@ pub trait MouseControllable {
 
     /// Scroll the mouse (wheel) left or right
     ///
-    /// Positive numbers for length lets the mouse wheel scroll to the right
-    /// and negative ones to the left. The value that is specified translates
-    /// to `lines` defined by the operating system and is essentially one 15°
-    /// (click)rotation on the mouse wheel. How many lines it moves depends
-    /// on the current setting in the operating system.
+    /// Positive numbers for `length` scroll to the right and negative ones to
+    /// the left. The value that is specified translates to `lines` defined
+    /// by the operating system and is essentially one 15° (click) rotation
+    /// on the mouse wheel. How many lines it moves depends on the current
+    /// setting in the operating system.
     ///
     /// # Example
     ///
@@ -225,11 +216,11 @@ pub trait MouseControllable {
 
     /// Scroll the mouse (wheel) up or down
     ///
-    /// Positive numbers for length lets the mouse wheel scroll down
-    /// and negative ones up. The value that is specified translates
-    /// to `lines` defined by the operating system and is essentially one 15°
-    /// (click)rotation on the mouse wheel. How many lines it moves depends
-    /// on the current setting in the operating system.
+    /// Positive numbers for `length` scroll down and negative ones up. The
+    /// value that is specified translates to `lines` defined by the
+    /// operating system and is essentially one 15° (click) rotation on the
+    /// mouse wheel. How many lines it moves depends on the current setting
+    /// in the operating system.
     ///
     /// # Example
     ///
@@ -240,7 +231,7 @@ pub trait MouseControllable {
     /// ```
     fn mouse_scroll_y(&mut self, length: i32);
 
-    /// Gets the (width, height) of the main display in screen coordinates
+    /// Get the (width, height) of the main display in screen coordinates
     /// (pixels). This currently only works on the main display
     ///
     /// # Example
@@ -253,7 +244,7 @@ pub trait MouseControllable {
     #[must_use]
     fn main_display_size(&self) -> (i32, i32);
 
-    /// Gets the location of the mouse in screen coordinates (pixels).
+    /// Get the location of the mouse in screen coordinates (pixels).
     ///
     /// # Example
     ///
@@ -367,13 +358,21 @@ pub enum Key {
     Raw(u16),
 }
 
-/// Representing an interface and a set of keyboard functions every
-/// operating system implementation _should_ implement.
+/// Contains functions to simulate key presses and to input text.
+///
+/// For the keyboard there are currently two modes you can use. The first mode
+/// is represented by the [`key_sequence`](KeyboardControllable::key_sequence)
+/// function. It's purpose is to simply write unicode characters. This is
+/// independent of the keyboard layout. Please note that
+/// you're not be able to use modifier keys like Control
+/// to influence the outcome. If you want to use modifier keys to e.g.
+/// copy/paste, use the Layout variant. Please note that this is indeed layout
+/// dependent.
 pub trait KeyboardControllable {
-    /// Types the string parsed with DSL.
+    /// Type the string parsed with DSL.
     ///
     /// Typing {+SHIFT}hello{-SHIFT} becomes HELLO.
-    /// TODO: Full documentation
+    /// Please have a look at the [dsl] module for more information.
     fn key_sequence_parse(&mut self, sequence: &str)
     where
         Self: Sized,
@@ -394,12 +393,10 @@ pub trait KeyboardControllable {
         dsl::eval(self, sequence)
     }
 
-    /// Types the string
-    ///
-    /// Emits keystrokes such that the given string is inputted.
-    ///
-    /// You can use many unicode here like: ❤️. This works
-    /// regardless of the current keyboardlayout.
+    /// Enter the text. You can use unicode here like: ❤️. This works
+    /// regardless of the current keyboardlayout. You cannot use this function
+    /// for entering shortcuts or something similar. For shortcuts, use the
+    /// key_click method instead.
     ///
     /// # Example
     ///
@@ -410,16 +407,15 @@ pub trait KeyboardControllable {
     /// ```
     fn key_sequence(&mut self, sequence: &str);
 
-    /// presses a given key down
+    /// Press down the given key
     fn key_down(&mut self, key: Key);
 
-    /// release a given key formally pressed down by
-    /// [`KeyboardControllable::key_down`]
+    /// Release a pressed down key
     fn key_up(&mut self, key: Key);
 
-    /// Much like the [`KeyboardControllable::key_down`] and
-    /// [`KeyboardControllable::key_up`] function they're just invoked
-    /// consecutively
+    /// Press and release the key. It is the same as calling the
+    /// [`KeyboardControllable::key_down`] and
+    /// [`KeyboardControllable::key_up`] functions consecutively
     fn key_click(&mut self, key: Key);
 }
 
