19d22191be4c4b0c321a47287d8d357b98380292
pentamassiv <pentamassiv@posteo.de>
2023-10-11 03:11:09 +0200

    macos: Made the types not public

--- src/macos/macos_impl.rs
+++ src/macos/macos_impl.rs
@@ -21,91 +21,72 @@ use crate::{
 #[link(name = "AppKit", kind = "framework")]
 extern "C" {}
 
-pub type CFDataRef = *const c_void;
+type CFDataRef = *const c_void;
 
 #[repr(C)]
-pub struct __TISInputSource;
-pub type TISInputSourceRef = *const __TISInputSource;
+struct __TISInputSource;
+type TISInputSourceRef = *const __TISInputSource;
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct __CFString([u8; 0]);
-pub type CFStringRef = *const __CFString;
-pub type Boolean = c_uchar;
-pub type UInt8 = c_uchar;
-pub type SInt32 = c_int;
-pub type UInt16 = c_ushort;
-pub type UInt32 = c_uint;
-pub type UniChar = UInt16;
-pub type UniCharCount = c_ulong;
+struct __CFString([u8; 0]);
+type CFStringRef = *const __CFString;
+type Boolean = c_uchar;
+type UInt8 = c_uchar;
+type SInt32 = c_int;
+type UInt16 = c_ushort;
+type UInt32 = c_uint;
+type UniChar = UInt16;
+type UniCharCount = c_ulong;
 
-pub type OptionBits = UInt32;
-pub type OSStatus = SInt32;
+type OptionBits = UInt32;
+type OSStatus = SInt32;
 
-pub type CFStringEncoding = UInt32;
+type CFStringEncoding = UInt32;
 
-pub const TRUE: c_uint = 1;
+const TRUE: c_uint = 1;
 
 #[allow(non_snake_case)]
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct UCKeyboardTypeHeader {
-    pub keyboardTypeFirst: UInt32,
-    pub keyboardTypeLast: UInt32,
-    pub keyModifiersToTableNumOffset: UInt32,
-    pub keyToCharTableIndexOffset: UInt32,
-    pub keyStateRecordsIndexOffset: UInt32,
-    pub keyStateTerminatorsOffset: UInt32,
-    pub keySequenceDataIndexOffset: UInt32,
+struct UCKeyboardTypeHeader {
+    keyboardTypeFirst: UInt32,
+    keyboardTypeLast: UInt32,
+    keyModifiersToTableNumOffset: UInt32,
+    keyToCharTableIndexOffset: UInt32,
+    keyStateRecordsIndexOffset: UInt32,
+    keyStateTerminatorsOffset: UInt32,
+    keySequenceDataIndexOffset: UInt32,
 }
 
 #[allow(non_snake_case)]
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct UCKeyboardLayout {
-    pub keyLayoutHeaderFormat: UInt16,
-    pub keyLayoutDataVersion: UInt16,
-    pub keyLayoutFeatureInfoOffset: UInt32,
-    pub keyboardTypeCount: UInt32,
-    pub keyboardTypeList: [UCKeyboardTypeHeader; 1usize],
+struct UCKeyboardLayout {
+    keyLayoutHeaderFormat: UInt16,
+    keyLayoutDataVersion: UInt16,
+    keyLayoutFeatureInfoOffset: UInt32,
+    keyboardTypeCount: UInt32,
+    keyboardTypeList: [UCKeyboardTypeHeader; 1usize],
 }
 
 #[allow(non_upper_case_globals)]
-pub const kUCKeyTranslateNoDeadKeysBit: _bindgen_ty_703 =
-    _bindgen_ty_703::kUCKeyTranslateNoDeadKeysBit;
+const kUCKeyTranslateNoDeadKeysBit: _bindgen_ty_703 = _bindgen_ty_703::kUCKeyTranslateNoDeadKeysBit;
 
 #[allow(non_camel_case_types)]
 #[repr(u32)]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum _bindgen_ty_703 {
+enum _bindgen_ty_703 {
     kUCKeyTranslateNoDeadKeysBit = 0,
 }
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct __CFAllocator([u8; 0]);
-pub type CFAllocatorRef = *const __CFAllocator;
-
-// #[repr(u32)]
-// #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-// pub enum _bindgen_ty_15 {
-//     kCFStringEncodingMacRoman = 0,
-//     kCFStringEncodingWindowsLatin1 = 1280,
-//     kCFStringEncodingISOLatin1 = 513,
-//     kCFStringEncodingNextStepLatin = 2817,
-//     kCFStringEncodingASCII = 1536,
-//     kCFStringEncodingUnicode = 256,
-//     kCFStringEncodingUTF8 = 134217984,
-//     kCFStringEncodingNonLossyASCII = 3071,
-//     kCFStringEncodingUTF16BE = 268435712,
-//     kCFStringEncodingUTF16LE = 335544576,
-//     kCFStringEncodingUTF32 = 201326848,
-//     kCFStringEncodingUTF32BE = 402653440,
-//     kCFStringEncodingUTF32LE = 469762304,
-// }
+struct __CFAllocator([u8; 0]);
+type CFAllocatorRef = *const __CFAllocator;
 
 #[allow(non_upper_case_globals)]
-pub const kCFStringEncodingUTF8: u32 = 134_217_984;
+const kCFStringEncodingUTF8: u32 = 134_217_984;
 
 #[allow(improper_ctypes)]
 #[link(name = "Carbon", kind = "framework")]
@@ -113,26 +94,21 @@ extern "C" {
     fn TISCopyCurrentKeyboardInputSource() -> TISInputSourceRef;
     fn TISCopyCurrentKeyboardLayoutInputSource() -> TISInputSourceRef;
 
-    //     extern void *
-    // TISGetInputSourceProperty(
-    //   TISInputSourceRef   inputSource,
-    //   CFStringRef         propertyKey)
-
     #[allow(non_upper_case_globals)]
     #[link_name = "kTISPropertyUnicodeKeyLayoutData"]
-    pub static kTISPropertyUnicodeKeyLayoutData: CFStringRef;
+    static kTISPropertyUnicodeKeyLayoutData: CFStringRef;
 
     #[allow(non_snake_case)]
-    pub fn TISGetInputSourceProperty(
+    fn TISGetInputSourceProperty(
         inputSource: TISInputSourceRef,
         propertyKey: CFStringRef,
     ) -> *mut c_void;
 
     #[allow(non_snake_case)]
-    pub fn CFDataGetBytePtr(theData: CFDataRef) -> *const UInt8;
+    fn CFDataGetBytePtr(theData: CFDataRef) -> *const UInt8;
 
     #[allow(non_snake_case)]
-    pub fn UCKeyTranslate(
+    fn UCKeyTranslate(
         keyLayoutPtr: *const UInt8, //*const UCKeyboardLayout,
         virtualKeyCode: UInt16,
         keyAction: UInt16,
@@ -145,10 +121,10 @@ extern "C" {
         unicodeString: *mut UniChar,
     ) -> OSStatus;
 
-    pub fn LMGetKbdType() -> UInt8;
+    fn LMGetKbdType() -> UInt8;
 
     #[allow(non_snake_case)]
-    pub fn CFStringCreateWithCharacters(
+    fn CFStringCreateWithCharacters(
         alloc: CFAllocatorRef,
         chars: *const UniChar,
         numChars: CFIndex,
@@ -156,13 +132,13 @@ extern "C" {
 
     #[allow(non_upper_case_globals)]
     #[link_name = "kCFAllocatorDefault"]
-    pub static kCFAllocatorDefault: CFAllocatorRef;
+    static kCFAllocatorDefault: CFAllocatorRef;
 
     #[allow(non_snake_case)]
-    pub fn CFStringGetLength(theString: CFStringRef) -> CFIndex;
+    fn CFStringGetLength(theString: CFStringRef) -> CFIndex;
 
     #[allow(non_snake_case)]
-    pub fn CFStringGetCString(
+    fn CFStringGetCString(
         theString: CFStringRef,
         buffer: *mut c_char,
         bufferSize: CFIndex,
