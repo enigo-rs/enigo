cf29929e21b66a44e544ef1a03cc7a28f02f35fe
pentamassiv <pentamassiv@posteo.de>
2024-08-16 00:00:24 +0200

    Try using Firefox on macOS too

--- tests/common/browser.rs
+++ tests/common/browser.rs
@@ -19,10 +19,10 @@ pub static BROWSER_INSTANCE: std::sync::LazyLock<Option<std::process::Child>> =
                 .expect("Failed to start Firefox on Windows")
         } else if cfg!(target_os = "macos") {
             // On macOS, use the "open" command to run Safari (Firefox is not preinstalled)
-            std::process::Command::new("open")
-                .args(["-a", "Safari", &url])
+            std::process::Command::new("firefox")
+                .args(["--kiosk", &url])
                 .spawn()
-                .expect("Failed to start Safari on macOS")
+                .expect("Failed to start Firefox in kiosk mode on macOS")
         } else {
             // On Linux, use the "firefox" command
             std::process::Command::new("firefox")
