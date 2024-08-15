2810c4848248fb9c73721a41304bd3de1f20589e
pentamassiv <pentamassiv@posteo.de>
2024-08-16 00:18:09 +0200

    Simplify the code

--- tests/common/browser.rs
+++ tests/common/browser.rs
@@ -1,8 +1,9 @@
+// Launch Firefox
+//
 // n.b. static items do not call [`Drop`] on program termination, so this won't
 // be deallocated. this is fine, as the OS can deallocate the terminated program
 // faster than we can free memory but tools like valgrind might report "memory
-// leaks" as it isn't obvious this is intentional. Launch Firefox on Linux and
-// Windows and launch Safari on macOS
+// leaks" as it isn't obvious this is intentional.
 pub static BROWSER_INSTANCE: std::sync::LazyLock<Option<std::process::Child>> =
     std::sync::LazyLock::new(|| {
         // Construct the URL
@@ -17,18 +18,11 @@ pub static BROWSER_INSTANCE: std::sync::LazyLock<Option<std::process::Child>> =
                 .args(["/C", "start", "firefox", "--kiosk", &url])
                 .spawn()
                 .expect("Failed to start Firefox on Windows")
-        } else if cfg!(target_os = "macos") {
-            // On macOS, use the "open" command to run Safari (Firefox is not preinstalled)
-            std::process::Command::new("firefox")
-                .args(["--kiosk", &url])
-                .spawn()
-                .expect("Failed to start Firefox in kiosk mode on macOS")
         } else {
-            // On Linux, use the "firefox" command
             std::process::Command::new("firefox")
                 .args(["--kiosk", &url])
                 .spawn()
-                .expect("Failed to start Firefox on Linux")
+                .expect("Failed to start Firefox")
         };
         Some(child)
     });
