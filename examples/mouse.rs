ec780149ab9f9c506bd194f7ceb450e3f2d56c2e
pentamassiv <pentamassiv@posteo.de>
2024-11-29 00:36:24 +0100

    Get values from the CI

--- examples/mouse.rs
+++ examples/mouse.rs
@@ -11,40 +11,40 @@ use std::time::Duration;
 fn main() {
     env_logger::try_init().ok();
     let wait_time = Duration::from_secs(2);
-    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    let mut enigo = Enigo::new(&Settings {
+        windows_subject_to_mouse_speed_and_acceleration_level: true,
+        ..Default::default()
+    })
+    .unwrap();
 
     thread::sleep(Duration::from_secs(4));
     println!("screen dimensions: {:?}", enigo.main_display().unwrap());
     println!("mouse location: {:?}", enigo.location().unwrap());
 
-    thread::sleep(wait_time);
+    enigo.move_mouse(0, 0, Abs).unwrap();
+    let detail = 1;
+    for i in 0..600 {
+        for _ in 0..detail {
+            enigo.move_mouse(i, 0, Rel).unwrap();
+        }
 
-    enigo.move_mouse(500, 200, Abs).unwrap();
-    thread::sleep(wait_time);
+        //thread::sleep(wait_time);
+        println!("{i}, {:?},", enigo.location().unwrap().0);
+        for _ in 0..detail {
+            enigo.move_mouse(-i, 0, Rel).unwrap();
+        }
+    }
+    println!("\n\ny");
 
-    enigo.button(Button::Left, Press).unwrap();
-    thread::sleep(wait_time);
+    for i in 0..600 {
+        for _ in 0..detail {
+            enigo.move_mouse(0, i, Rel).unwrap();
+        }
 
-    enigo.move_mouse(100, 100, Rel).unwrap();
-    thread::sleep(wait_time);
-
-    enigo.button(Button::Left, Release).unwrap();
-    thread::sleep(wait_time);
-
-    enigo.button(Button::Left, Click).unwrap();
-    thread::sleep(wait_time);
-
-    enigo.scroll(2, Horizontal).unwrap();
-    thread::sleep(wait_time);
-
-    enigo.scroll(-2, Horizontal).unwrap();
-    thread::sleep(wait_time);
-
-    enigo.scroll(2, Vertical).unwrap();
-    thread::sleep(wait_time);
-
-    enigo.scroll(-2, Vertical).unwrap();
-    thread::sleep(wait_time);
-
-    println!("mouse location: {:?}", enigo.location().unwrap());
+        //thread::sleep(wait_time);
+        println!("{i}, {:?},", enigo.location().unwrap().0);
+        for _ in 0..detail {
+            enigo.move_mouse(0, -i, Rel).unwrap();
+        }
+    }
 }
