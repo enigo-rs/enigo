493e568fbfc42c729ed131af3e4263e192ba6a66
pentamassiv <pentamassiv@posteo.de>
2024-11-29 04:37:58 +0100

    CI: Improved test coverage of mouse
    Test relative and absolute mouse moves in the CI. Run more unit tests concerning mouse movement as well

--- tests/integration_browser.rs
+++ tests/integration_browser.rs
@@ -1,6 +1,12 @@
 use enigo::{
+    Axis::{Horizontal, Vertical},
+    // Button,
+    Coordinate::{Abs, Rel},
     Direction::{Click, Press, Release},
-    Key, Keyboard, Settings,
+    Key,
+    Keyboard,
+    Mouse as _,
+    Settings,
 };
 
 mod common;
@@ -10,29 +16,30 @@ use common::enigo_test::EnigoTest as Enigo;
 fn integration_browser_events() {
     let mut enigo = Enigo::new(&Settings::default());
 
-    enigo.text("TestText❤️").unwrap(); // Fails on Windows (Message is empty???)
+    enigo.text("TestText❤️").unwrap();
     enigo.key(Key::F1, Click).unwrap();
     enigo.key(Key::Control, Click).unwrap();
     enigo.key(Key::Backspace, Click).unwrap();
-    enigo.key(Key::PageUp, Click).unwrap(); // Failing on Windows
+    enigo.key(Key::PageUp, Click).unwrap();
 
     enigo.key(Key::Backspace, Press).unwrap();
     enigo.key(Key::Backspace, Release).unwrap();
 
-    println!("Test mouse"); /*
-                            enigo.button(Button::Left, Click).unwrap();
-                            enigo.move_mouse(100, 100, Abs).unwrap();
-                            enigo.move_mouse(200, 200, Abs).unwrap();
-                            // let (x, y) = enigo.location().unwrap();
-                            // assert_eq!((200, 200), (x, y));
-                            // Relative moves fail on Windows
-                            // For some reason the values are wrong
-                            // enigo.move_mouse(20, 20, Rel).unwrap();
-                            // enigo.move_mouse(-20, 20, Rel).unwrap();
-                            // enigo.move_mouse(20, -20, Rel).unwrap();
-                            // enigo.move_mouse(-20, -20, Rel).unwrap();
-                            // enigo.scroll(1, Vertical).unwrap();
-                            // enigo.scroll(1, Horizontal).unwrap(); Fails on Windows
-                            enigo.main_display().unwrap();
-                            enigo.location().unwrap(); */
+    println!("Test mouse");
+    // enigo.button(Button::Left, Click).unwrap();
+    enigo.move_mouse(100, 100, Abs).unwrap();
+    enigo.move_mouse(200, 200, Abs).unwrap();
+    let (x, y) = enigo.location().unwrap();
+    assert_eq!((200, 200), (x, y));
+    enigo.move_mouse(20, 20, Rel).unwrap();
+    enigo.move_mouse(-20, 20, Rel).unwrap();
+    enigo.move_mouse(20, -20, Rel).unwrap();
+    enigo.move_mouse(-20, -20, Rel).unwrap();
+
+    // Stalls on Windows, macOS and Linux with x11rb
+    // enigo.scroll(1, Vertical).unwrap();
+    // enigo.scroll(1, Horizontal).unwrap();
+
+    enigo.main_display().unwrap();
+    enigo.location().unwrap();
 }
