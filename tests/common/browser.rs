d823b4f7a487bf5c210b5bd445f2d5b0b2b6b196
pentamassiv <pentamassiv@posteo.de>
2025-10-05 22:16:59 +0200

    tests: Suppress the "Welcome to Firefox" screen
    The popup breaks the integration tests, because all input goes to it.
    Fixes https://github.com/enigo-rs/enigo/issues/446.

--- tests/common/browser.rs
+++ tests/common/browser.rs
@@ -6,28 +6,35 @@
 // leaks" as it isn't obvious this is intentional.
 pub static BROWSER_INSTANCE: std::sync::LazyLock<Option<std::process::Child>> =
     std::sync::LazyLock::new(|| {
-        // Construct the URL
-        let url = format!(
-            "file://{}/tests/index.html",
-            std::env::current_dir().unwrap().to_str().unwrap()
-        );
+        // Project root
+        let cwd = std::env::current_dir().unwrap();
+
+        // Paths
+        let index = cwd.join("tests/index.html");
+        let url = format!("file://{}", index.to_str().unwrap());
+        let profile = cwd.join("tests/firefox-profile");
+        let profile = profile.to_str().unwrap();
 
         let child = if cfg!(target_os = "windows") {
             // On Windows, use cmd.exe to run the "start" command
             std::process::Command::new("cmd")
-                .args(["/C", "start", "firefox", "--kiosk", &url])
+                .args([
+                    "/C", "start", "firefox", "--kiosk", &url, "-profile", profile,
+                ])
                 .spawn()
                 .expect("Failed to start Firefox")
         } else if cfg!(target_os = "macos") {
             // On macOS, use the "open" command to run Firefox
             std::process::Command::new("open")
-                .args(["-a", "Firefox", "--args", "--kiosk", &url])
+                .args([
+                    "-a", "Firefox", "--args", "--kiosk", &url, "-profile", profile,
+                ])
                 .spawn()
                 .expect("Failed to start Firefox")
         } else {
             // On Linux, use the "firefox" command
             std::process::Command::new("firefox")
-                .args(["--kiosk", &url])
+                .args(["--kiosk", &url, "-profile", profile])
                 .spawn()
                 .expect("Failed to start Firefox")
         };
