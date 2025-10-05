9a730dfb7a2c0eb8a1d43931ed52f987d948959d
pentamassiv <pentamassiv@posteo.de>
2025-10-05 22:16:59 +0200

    tests: Suppress the "Welcome to Firefox" screen
    The popup breaks the integration tests, because all input goes to it.
    Fixes https://github.com/enigo-rs/enigo/issues/446.

---
+++ tests/firefox-profile/user.js
@@ -0,0 +1,10 @@
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("startup.homepage_override_url", "");
+user_pref("browser.startup.firstrunSkipsHomepage", true);
+
+user_pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 2);
+user_pref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "1759697962369");
+user_pref("distribution.canonical-002.bookmarksProcessed", true);
