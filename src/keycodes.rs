0c890ef31949490e077818de84868a8272ffca46
pentamassiv <pentamassiv@posteo.de>
2025-10-06 19:50:26 +0200

    drop: serde aliases to match Firefox names

--- src/keycodes.rs
+++ src/keycodes.rs
@@ -179,6 +179,7 @@ pub enum Key {
     Delete,
     Divide,
     /// down arrow key
+    #[cfg_attr(feature = "serde", serde(alias = "ArrowDown"))]
     DownArrow,
     #[cfg(target_os = "macos")]
     Eject,
@@ -371,6 +372,7 @@ pub enum Key {
     #[cfg_attr(feature = "serde", serde(alias = "ControlLeft"))]
     LControl,
     /// left arrow key
+    #[cfg_attr(feature = "serde", serde(alias = "ArrowLeft"))]
     LeftArrow,
     #[cfg(all(unix, not(target_os = "macos")))]
     Linefeed,
@@ -421,6 +423,7 @@ pub enum Key {
     NonConvert,
     #[cfg(target_os = "windows")]
     None,
+    #[cfg_attr(feature = "serde", serde(alias = "NumLock"))]
     Numlock,
     Numpad0,
     Numpad1,
@@ -539,6 +542,7 @@ pub enum Key {
     /// return key
     Return,
     /// right arrow key
+    #[cfg_attr(feature = "serde", serde(alias = "ArrowRight"))]
     RightArrow,
     #[cfg(target_os = "windows")]
     RMenu,
@@ -581,6 +585,7 @@ pub enum Key {
     #[cfg(all(unix, not(target_os = "macos")))]
     Undo,
     /// up arrow key
+    #[cfg_attr(feature = "serde", serde(alias = "ArrowUp"))]
     UpArrow,
     #[cfg(target_os = "macos")]
     VidMirror,
